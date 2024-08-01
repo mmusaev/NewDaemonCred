c79db280840edff3044d88b6a640bdf4f8708474
mmusaev <9410421+mmusaev@users.noreply.github.com>
2024-08-01 15:34:51 -0500

    use cert store

--- DaemonClientCred/Program.cs
+++ DaemonClientCred/Program.cs
@@ -11,27 +11,21 @@ namespace DaemonClientCred
 {
     class Program
     {
-        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
-        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
-        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
-        private static string keyVaultUri = ConfigurationManager.AppSettings["ida:KeyVaultUri"];
-        private static string certName = ConfigurationManager.AppSettings["ida:CertName"];
+        private static readonly string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static readonly string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static readonly string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static readonly string certName = ConfigurationManager.AppSettings["ida:CertName"];
+        private static readonly string authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly string audienceUri = ConfigurationManager.AppSettings["ida:AudienceUri"];
+        private static readonly string funcAppUrl = ConfigurationManager.AppSettings["ida:FunctionUrl"];
 
-        static string authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
-
-        private static HttpClient httpClient = new HttpClient();
-
-        private static string audienceUri = ConfigurationManager.AppSettings["ida:AudienceUri"];
-
-        private static string funcAppUrl = ConfigurationManager.AppSettings["ida:FunctionUrl"];
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             try
             {
-                var result = CallAPI().Result;
+                var result = await CallAPI();
                 Console.WriteLine($"Here is the result {result}");
-
             }
             catch (Exception ex)
             {
@@ -42,36 +36,36 @@ namespace DaemonClientCred
 
         static async Task<string> CallAPI()
         {
-            AuthenticationResult result = await GetAccessTokenWithMSAL(audienceUri);
+            var result = await GetAccessTokenWithMSAL(audienceUri);
             if (result == null)
             {
                 Console.WriteLine("Canceling attempt to call func app.\n");
                 return null;
             }
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+
+            // Create the multipart form data content
+            var content = new MultipartFormDataContent();
+
+            // In-memory data
+            byte[] inMemoryData = System.Text.Encoding.UTF8.GetBytes("This is the in-memory data content.");
+            var fileContent = new ByteArrayContent(inMemoryData)
+            {
+                Headers = { ContentType = new MediaTypeHeaderValue("application/octet-stream") }
+            };
+            content.Add(fileContent, "file", "inMemoryData.txt"); // Replace "file" with the name expected by the API
+
+            // Send the POST request
+            var response = await httpClient.PostAsync(funcAppUrl, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
             else
             {
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
-
-                // Create the multipart form data content
-                var content = new MultipartFormDataContent();
-
-                // In-memory data
-                byte[] inMemoryData = System.Text.Encoding.UTF8.GetBytes("This is the in-memory data content.");
-                var fileContent = new ByteArrayContent(inMemoryData);
-                fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-                content.Add(fileContent, "file", "inMemoryData.txt"); // Replace "file" with the name expected by the API
-
-                // Send the POST request
-                HttpResponseMessage response = await httpClient.PostAsync(funcAppUrl, content);
-                if (response.IsSuccessStatusCode)
-                {
-                    return await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    Console.WriteLine($"Failed to call func app.\nError:  {response.ReasonPhrase}\n");
-                    return null;
-                }
+                Console.WriteLine($"Failed to call func app.\nError:  {response.ReasonPhrase}\n");
+                return null;
             }
         }
 
@@ -80,7 +74,7 @@ namespace DaemonClientCred
             var cert = GetCertificateFromLocalStore(certName);
             if (cert == null)
             {
-                Console.WriteLine($"Cannot find active certificate '{certName}' in Azure Key Vault. Please check configuration");
+                Console.WriteLine($"Cannot find active certificate '{certName}' in the local store. Please check configuration");
                 return null;
             }
 
@@ -98,10 +92,10 @@ namespace DaemonClientCred
 
         private static X509Certificate2 GetCertificateFromLocalStore(string certName)
         {
-            using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
             {
                 store.Open(OpenFlags.ReadOnly);
-                X509Certificate2Collection certCollection = store.Certificates.Find(X509FindType.FindBySubjectName, certName, false);
+                var certCollection = store.Certificates.Find(X509FindType.FindBySubjectName, certName, false);
                 if (certCollection.Count > 0)
                 {
                     return certCollection[0];
